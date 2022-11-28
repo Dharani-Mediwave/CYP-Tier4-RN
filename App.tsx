0f434bf89f65db5ed55444b786f750660838431b
Dharani-Mediwave <dharani@mindwaveventures.com>
2022-11-28 17:02:59 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+import AppNavigator from "./src/navigation/Navigator";
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <AppNavigator />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
